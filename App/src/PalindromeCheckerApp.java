2b25871e303652576f74a09d13c55a36acee7ee4
Nitya Mehrotra <nityamehrotra009@gmail.com>
2026-02-27 08:37:56 +0530

    Nitya Mehrotra added UC-2

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,16 @@
+import java.util.*;
+    public class PalindromeCheckerApp {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            boolean palindrome = true;
+            System.out.print("Input text: ");
+            String text = sc.nextLine();
+            for(int i = 0; i < text.length() / 2; i++) {
+                if(text.charAt(i) != text.charAt(text.length() - i - 1)) {
+                    palindrome = false;
+                    break;
+                }
+            }
+            System.out.println("Is it a Palindrome? : " +  palindrome);
+        }
+    }
